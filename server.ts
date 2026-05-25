61ca120f3bf5c39b7163b355db6967275a8df463
Erick Lyndon Redondo <redondosuicoericklyndon08@gmail.com>
2026-05-25 22:01:29 +0800

    fix server.ts for Vercel serverless

--- server.ts
+++ server.ts
@@ -45,4 +45,9 @@ app.use(errorHandler);
 
 // start server
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
-app.listen(port, () => console.log('Server listening on port ' + port));
+
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(port, () => console.log('Server listening on port ' + port));
+}
+
+export default app;
